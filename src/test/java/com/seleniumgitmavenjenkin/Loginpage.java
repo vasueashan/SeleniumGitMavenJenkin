9c0cfcf17086c6d9a0c8c63b5a4ec508edd2c080
vasu <vsun21@gmail.com>
2022-01-24 20:34:03 +0530

    commit all

---
+++ src/test/java/com/seleniumgitmavenjenkin/Loginpage.java
@@ -0,0 +1,32 @@
+package com.seleniumgitmavenjenkin;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Loginpage {
+
+	@Test
+	public void loginpage()
+	{
+		System.out.println("Testcase-1");
+		WebDriverManager.chromiumdriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.makemytrip.com/");
+		driver.quit();		
+		
+	}
+	@Test
+	public void loginpage1()
+	{
+		System.out.println("Testcase-2");
+		WebDriverManager.chromiumdriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.makemytrip.com/");
+		driver.quit();		
+		
+	}
+
+}
---
+++ src/test/java/com/seleniumgitmavenjenkin/Username.java
@@ -0,0 +1,32 @@
+package com.seleniumgitmavenjenkin;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Username {
+
+	@Test
+	public void userlogin()
+	{
+		System.out.println("UserloginTestcase-1");
+		WebDriverManager.chromiumdriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.makemytrip.com/");
+		driver.quit();		
+		
+	}
+	@Test
+	public void userlogin1()
+	{
+		System.out.println("userloginTestcase-2");
+		WebDriverManager.chromiumdriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.makemytrip.com/");
+		driver.quit();		
+		
+	}
+
+}
